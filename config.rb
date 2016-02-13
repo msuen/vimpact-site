ed9cbb2484931628e6e158e6a79511a4987663ac
Michael Suen <msuen89@gmail.com>
2016-02-13 13:08:47 -0500

    Attempting to fix directory

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/vimpact-site/"
+http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
@@ -12,7 +12,7 @@ fonts_dir = "fonts"
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 line_comments = false
 color_output = false
